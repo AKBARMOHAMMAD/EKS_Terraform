05930d22ac5a0727f304af08d57d96c1405c978a
AKBAR MOHAMMAD <43230334+AKBARMOHAMMAD@users.noreply.github.com>
2021-09-22 00:20:47 +0530

    Update eks_nodegroup.tf

--- eks_nodegroup.tf
+++ eks_nodegroup.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "eks_cluster" {
-  name               = "eks_cluster"
+resource "aws_iam_role" "node_group" {
+  name               = "eks_node_group"
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -7,8 +7,8 @@ resource "aws_iam_role" "eks_cluster" {
     {
       "Effect": "Allow",
       "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
+        "Service": "ec2.amazonaws.com"
+      }, 
       "Action": "sts:AssumeRole"
     }
   ]
@@ -16,29 +16,57 @@ resource "aws_iam_role" "eks_cluster" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster.name
+resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_group.name
 }
 
-resource "aws_eks_cluster" "Akbar-eks" {
-  name     = "Akbar-eks"
-  role_arn = aws_iam_role.eks_cluster.arn
-  version  = "1.18"
+resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_group.name
+}
 
-  vpc_config {
-    endpoint_public_access  = true
-    endpoint_private_access = false
+resource "aws_iam_role_policy_attachment" "amazon_ec2_container_registry" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_group.name
+
+}
+
+resource "aws_eks_node_group" "nodes" {
+    cluster_name = aws_eks_cluster.Akbar-eks.name
+    node_group_name = "nodes"
+    node_role_arn = aws_iam_role.node_group.arn
+
+    subnet_ids = [
+        aws_subnet.private-subnet-1.id,
+        aws_subnet.private-subnet-2.id
+    ]
+
+    scaling_config {
+        desired_size = 1
+        max_size = 10
+        min_size = 1
+    }
+     ami_type = "AL2_x86_64"
+     capacity_type = "ON_DEMAND"
+     disk_size = 20
+     force_update_version = false
+     instance_types = ["t3.small"]
 
 
-  subnet_ids = [
-    aws_subnet.pub-subnet-1.id,
-    aws_subnet.pub-subnet-2.id,
-    aws_subnet.private-subnet-1.id,
-    aws_subnet.private-subnet-2.id
-  ]
-  }
-  depends_on = [
-    aws_iam_role_policy_attachment.amazon_eks_cluster_policy
-  ]
+     
+    labels = {
+      role = "nodes"
+    }
+
+    version = "1.18"
+
+    depends_on = [
+      aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+      aws_iam_role_policy_attachment.amazon_eks_cni_policy,
+      aws_iam_role_policy_attachment.amazon_ec2_container_registry,
+    ]
+
+
+  
 }
